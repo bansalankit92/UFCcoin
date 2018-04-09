ac5400ba2a0cfeb00a93e35b0b50284ed418085c
abansal <abansal.shutterfly.com>
2018-04-09 11:08:56 +0530

    added few UI changes

--- src/app/component/balance-of/balance-of.component.ts
+++ src/app/component/balance-of/balance-of.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenService } from 'app/service/token.service';
+import { UtilService } from '../../service/util.service';
 
 @Component({
   selector: 'app-balance-of',
@@ -10,7 +11,7 @@ export class BalanceOfComponent implements OnInit {
 
   balance: number;
   accountAddress;
-
+message;
   constructor(private tokenService: TokenService) {
 
   }
@@ -19,13 +20,25 @@ export class BalanceOfComponent implements OnInit {
   }
   
   search() {
+    if(UtilService.isValidAddress(this.accountAddress)){
+      this.message = '';
      this.tokenService.balanceOf(this.accountAddress).subscribe(res=>{
       console.log(res);
       this.balance=res;
+      if(this.balance>0){
+        alert('You have ' + this.balance/1000000000000000000 + ' UFC coins :)');
+      }else{
+          alert('You have 0 UFC coins :( Please get UFC coins');
+
+        }
     }, err => {
       console.log('err in search', err);
+      this.balance=0;
+      alert('Some error occurred. You might have not installed Metamask');
     });
+   }else{
+this.message='Please enter correct Ethereum address';
    }
-
+  }
 
 }
